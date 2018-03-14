976b11d9dcadea5d645f4096f297ca78eb61b401
Patrick <patrick.floissac@gmail.com>
2018-03-14 09:16:40 +0100

    J-2 ouverture :)

--- app.js
+++ app.js
@@ -462,8 +462,8 @@ app.post('/register', function(req, res) {
   var d2 = CryptoJS.AES.decrypt(encPassword2, userId).toString(CryptoJS.enc.Utf8);
 
   // vérification dez champs
-  //req.checkBody('magic', 'Le mot magique est obligatoire').trim().equals('svp');
-  req.checkBody('magic', 'LE SITE VA BIENTOT OUVRIR').trim().equals('install');
+  req.checkBody('magic', 'Le mot magique est obligatoire').trim().equals('svp');
+  //req.checkBody('magic', 'LE SITE VA BIENTOT OUVRIR').trim().equals('install');
   req.checkBody('userId', 'L\'identifiant doit être au minimum un trigramme. Il peut contenir des caractères minuscules, des chiffres ou _').trim().matches('[a-z][a-z0-9_]{1,}[a-z]');
   req.checkBody('userName', 'Le nom/prénom est obligatoire').trim().notEmpty();
   req.checkBody('email', 'L`\'email est obligatoire (mettez un email où Guillaume peut vous contacter)').trim().notEmpty();
